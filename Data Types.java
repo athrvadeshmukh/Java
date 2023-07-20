10383f1cf533b7bf84e008495ebd4f19511d7ad6
Athrva Deshmukh <112002659+athrvadeshmukh@users.noreply.github.com>
2023-07-20 14:35:08 +0530

    Create Data Types.java

---
+++ Data Types.java
@@ -0,0 +1,23 @@
+public class DataTypes{
+    public static void main(String []args)
+    {
+    byte a= 45;
+    byte b=10;
+    long c= 234566786;
+    int sum=a+b;
+    float Num1= 2.9f;
+    double Num2= 3.99879484844843;
+    char Word1= 'A';
+    char Word2= '@';
+
+
+        System.out.println("Char1 Data = " + Word1);
+        System.out.println("Char2 Data = " + Word2);
+        System.out.println("Double Data = " + Num2);
+        System.out.println("Float Data = " + Num1);
+        System.out.println("Byte Data SUM = " + sum);
+        System.out.println("Long Data = " +c);
+
+    }
+
+}
