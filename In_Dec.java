3bf5fb36583ad730e52af9c9d3e54da40506280a
Athrva Deshmukh <112002659+athrvadeshmukh@users.noreply.github.com>
2023-07-30 16:11:28 +0530

    Create In_Dec.java

---
+++ In_Dec.java
@@ -0,0 +1,12 @@
+class In_Dec{
+    public static void main(String[] args)
+    {
+        int a = 5;
+        int b = a++ + ++a + ++a + a++ + --a + a--;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+    
+}
