9028ead542498e69eb0ec9399f05be8483b287b8
Athrva Deshmukh <112002659+athrvadeshmukh@users.noreply.github.com>
2023-08-03 16:59:09 +0530

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,21 @@
+public class Ternary
+{
+    public static void main (String[] args)
+    {
+        int a=30;
+        int b=20;
+        int c=40;
+
+        int res = (a<b)? a: b;
+        System.out.println(res);
+       
+        String result = (a<b)? "a is less" : "b is less";
+        System.out.println(result);
+
+        int num = (a<b)? (a<c? a: b) : (b<c? b:c);
+        System.out.println(num);
+
+
+    }
+
+}
