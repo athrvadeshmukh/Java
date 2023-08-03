0cf00dd67358faeab42fa534a2c63ab94a71a747
Athrva Deshmukh <112002659+athrvadeshmukh@users.noreply.github.com>
2023-08-03 17:29:11 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,23 @@
+public class Loop 
+{
+    public static void main(String[] args)
+    {
+        for (int i=0; i<10; i++)
+        {
+            System.out.println("*");
+        }
+    }
+}
+
+/* OUTPUT
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+ */
