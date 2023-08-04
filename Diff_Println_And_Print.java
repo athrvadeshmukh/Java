cb8e6041e118a5aa1508f186faa1dbac62b36fa3
Athrva Deshmukh <112002659+athrvadeshmukh@users.noreply.github.com>
2023-08-04 14:29:47 +0530

    Add files via upload

---
+++ Diff_Println_And_Print.java
@@ -0,0 +1,21 @@
+public class Diff_Println_And_Print
+{
+    public static void main(String[] args)
+    {
+        System.out.println("System.out.println");
+
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        
+        System.out.println("System.out.print");
+
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+        System.out.print("*");
+
+    }
+}
