6b6e7aeaca369b10dd8d183e4ee7f1d7cda06389
Athrva Deshmukh <112002659+athrvadeshmukh@users.noreply.github.com>
2023-08-04 13:10:58 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,22 @@
+public class Switch
+{
+    public static void main(String[] args)
+    {
+        int num=0;
+        System.out.println("select 1");
+        System.out.println("select 2");
+        System.out.println("select 3");
+        System.out.println("select random number");
+
+        switch(num)
+        {
+            case 1: System.out.println("Hello World");
+            break;
+            case 2: System.out.println("This is switch case program");
+            break;
+            case 3: System.out.println("Made by using java");
+            break;
+            default: System.out.println("Default");
+        }
+    }
+}
